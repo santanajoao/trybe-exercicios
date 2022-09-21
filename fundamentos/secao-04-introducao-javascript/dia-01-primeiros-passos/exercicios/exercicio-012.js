c3df56c504e2c8e3fb04dc93c836e03b530d6df6
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-21 18:53:50 -0300

    Exercício 12 da seção 04 dia 01 adicionado

---
+++ fundamentos/secao-04-introducao-javascript/dia-01-primeiros-passos/exercicios/exercicio-012.js
@@ -0,0 +1,10 @@
+const numberA = 31;
+const numberB = 39;
+const numberC = 69;
+const numberSum = numberA + numberB + numberC;
+
+if (numberSum % 2 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
