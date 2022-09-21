e23aeb5a51b9d822e0c5d96375b7436f15876ff1
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-21 18:46:39 -0300

    Exercício 11 da seção 04 dia 01 adicionado

---
+++ fundamentos/secao-04-introducao-javascript/dia-01-primeiros-passos/exercicios/exercicio-011.js
@@ -0,0 +1,15 @@
+const percentualGrade = 87;
+
+if (percentualGrade >= 90) {
+  console.log('A');
+} else if (percentualGrade >= 80) {
+  console.log('B');
+} else if (percentualGrade >= 70) {
+  console.log('C');
+} else if (percentualGrade >= 60) {
+  console.log('D');
+} else if (percentualGrade >= 50) {
+  console.log('E');
+} else if (percentualGrade < 50) {
+  console.log('F');
+} 
