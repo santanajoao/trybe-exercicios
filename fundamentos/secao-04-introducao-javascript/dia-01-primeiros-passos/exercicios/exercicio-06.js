5c0c68ce39cbe426c46a07d111aeeb38390f1489
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-21 18:03:38 -0300

    Exercício 07 da seção 04 dia 01 adicionado

--- fundamentos/secao-04-introducao-javascript/dia-01-primeiros-passos/exercicios/exercicio-06.js
+++ fundamentos/secao-04-introducao-javascript/dia-01-primeiros-passos/exercicios/exercicio-06.js
@@ -3,8 +3,6 @@ const numeroB = 7;
 
 if (numeroA > numeroB) {
   console.log(numeroA);
-} else if (numeroA < numeroB) {
-  console.log(numeroB);
 } else {
-  console.log('Os números são iguais.')
+  console.log(numeroB);
 }
