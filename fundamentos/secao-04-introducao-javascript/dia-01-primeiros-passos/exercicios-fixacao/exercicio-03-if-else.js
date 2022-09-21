7b0224173daa3291b06e84a4cf6c19ef06a84a2a
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-21 16:04:45 -0300

    exercício de fixação 04 da seção 04 adicionado

--- fundamentos/secao-04-introducao-javascript/dia-01-primeiros-passos/exercicios-fixacao/exercicio-03-if-else.js
+++ fundamentos/secao-04-introducao-javascript/dia-01-primeiros-passos/exercicios-fixacao/exercicio-03-if-else.js
@@ -1,8 +1,8 @@
-const notaCandidata = 87;
+const notaCandidata = 83;
 
 if (notaCandidata >= 80) {
   console.log("Parabéns, você foi aprovada(o)!")
-} else if (60 <= notaCandidata < 80) {
+} else if (notaCandidata >= 60 && notaCandidata < 80) {
   console.log("Você está na nossa lista de espera.")
 } else {
   console.log("Você foi reprovada(o).")
