56bccc603cb44f0d45291da3d7318eae3d520ac4
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-22 15:33:12 -0300

    Exercícios 01 e 02 da seção 04 dia 02 adicionados

---
+++ fundamentos/secao-04-introducao-javascript/dia-02-array-loop-for/exercicios/exercicio-001.js
@@ -0,0 +1,57 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let arrayValueSum = 0;
+let mediaAritimetica = null;
+let greaterValue = null;
+let lowerValue = null;
+let numberOfOdd = 0;
+
+for (number of numbers) {
+  // percorra e imprima os valores da array
+  console.log(number);
+
+  // some todos os valores da array
+  arrayValueSum += number;
+
+  // descubra qual o maior valor do array
+  if (greaterValue === null || number > greaterValue) {
+    greaterValue = number;
+  }
+
+  // descubra qual o menor valor do array
+  if (lowerValue === null || number < lowerValue) {
+    lowerValue = number;
+  }
+  
+  if (number % 2 !== 0) {
+    numberOfOdd += 1;
+  }
+}
+
+// imprima a soma de todos os valores da array
+console.log('Soma dos elementos:', arrayValueSum);
+
+// calcule e imprima a média aritimética
+mediaAritimetica = arrayValueSum / numbers.length;
+console.log('Média Aritimética:', mediaAritimetica);
+
+/* caso a media seja maior que vinte, imprima "valor maior que 20".
+caso não, imprima "valor menor ou igual a 20" */
+if (mediaAritimetica > 20) {
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}
+
+// imprima o maior valor do array
+console.log('Maior valor:', greaterValue);
+
+/* descubra quantos valores ímpares existem no array.
+caso não exista, imprima "nenhum valor impar encontrado */
+if (numberOfOdd === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log('Número de valores ímpares:', numberOfOdd);
+}
+
+// imprima o menor valor do array
+console.log('Menor valor:', lowerValue);
---
+++ fundamentos/secao-04-introducao-javascript/dia-02-array-loop-for/exercicios/exercicio-002.js
@@ -0,0 +1,12 @@
+// crie um array que vá de 1 até 25 e imprima o resultado
+let numbers = [];
+
+for (let number = 1; number <= 25; number += 1) {
+  numbers.push(number);
+}
+console.log(numbers);
+
+// imprima o resultado da divisão de cada um dos elementos por 2
+for (number of numbers) {
+  console.log(number, "/ 2 =", number / 2);
+}
