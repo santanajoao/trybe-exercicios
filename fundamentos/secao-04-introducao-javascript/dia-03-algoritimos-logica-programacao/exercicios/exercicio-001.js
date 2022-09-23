5816291c6ecb6f82eeceefa81bb99f3640ea7b21
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-23 15:44:42 -0300

    Exercício 001 da seção 04 dia 03 adicionado

---
+++ fundamentos/secao-04-introducao-javascript/dia-03-algoritimos-logica-programacao/exercicios/exercicio-001.js
@@ -0,0 +1,12 @@
+const fatorialDe = 4;
+const numeroPositivo = fatorialDe >= 0;
+let resultadoFatorial = 1;
+
+if (numeroPositivo) {
+  for (let fator = 1; fator <= fatorialDe; fator += 1) {
+    resultadoFatorial *= fator;
+  }
+  console.log(resultadoFatorial);
+} else {
+  console.log('erro: não existe fatorial para números negativos')
+}
