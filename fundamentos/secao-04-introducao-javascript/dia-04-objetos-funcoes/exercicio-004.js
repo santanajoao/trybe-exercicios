4c3e32757b35db1c6fc891db80e72dec0d504a08
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-25 17:42:52 -0300

    Exercício de fixação 04 da seção 04 dia 04 adicionado

---
+++ fundamentos/secao-04-introducao-javascript/dia-04-objetos-funcoes/exercicio-004.js
@@ -0,0 +1,28 @@
+function soma(a, b) {
+  return a + b;
+}
+
+function subtrai(a, b) {
+  return a - b;
+}
+
+function multiplica(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function restoDivisao(a, b) {
+  return a % b;
+}
+
+const a = 21;
+const b = 7;
+
+console.log(soma(a, b));
+console.log(subtrai(a, b));
+console.log(multiplica(a, b));
+console.log(divide(a, b));
+console.log(restoDivisao(a, b));
