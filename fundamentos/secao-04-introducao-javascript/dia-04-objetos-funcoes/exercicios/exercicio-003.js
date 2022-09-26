c110b46a1fcac1900ddfabe11c4b60bd5708304e
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-25 21:36:36 -0300

    Exercicio 3 da Seção 04 dia 04 adicionado

---
+++ fundamentos/secao-04-introducao-javascript/dia-04-objetos-funcoes/exercicios/exercicio-003.js
@@ -0,0 +1,10 @@
+function palindromo(string) {
+  const comprimento = string.length;
+  const indiceMeio = Math.floor(comprimento / 2);
+  for (let index = 0; index <= indiceMeio; index += 1) {
+    if (string[index] !== string[comprimento - index - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
