2db7419f0d4a0b161fc7f6355584f4b11a00da4f
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-26 12:54:48 -0300

    Exercicio bônus 03 da seção 04 dia 04 adicionado

---
+++ fundamentos/secao-04-introducao-javascript/dia-04-objetos-funcoes/exercicios-bonus/exercicio-003.js
@@ -0,0 +1,44 @@
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+let quantidade = {};
+let stringImpressa = 'Sua cesta possui: ';
+
+function recorrencia(lista) {
+  let frutaRecorrencia = {};
+  for (valor of lista) {
+    if (!frutaRecorrencia[valor]) {
+      frutaRecorrencia[valor] = 1;
+    } else {
+      frutaRecorrencia[valor] += 1;
+    }
+  }
+  return frutaRecorrencia;
+}
+
+
+quantidade = recorrencia(basket);
+for (fruta in quantidade) {
+  if (stringImpressa !== 'Sua cesta possui: ') {
+    stringImpressa += ', ';
+  }
+  stringImpressa += ( + quantidade[fruta] + ' ' + fruta + 's');
+}
+
+console.log(stringImpressa);
