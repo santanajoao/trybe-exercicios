098bb8a868687cde38295048a36bb73ccd2e0144
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-26 15:11:52 -0300

    Exercicio bônus 04 da seção 04 dia 04 adicionado

---
+++ fundamentos/secao-04-introducao-javascript/dia-04-objetos-funcoes/exercicios-bonus/exercicio-004.js
@@ -0,0 +1,49 @@
+const moradores = {
+  blocoUm: [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+
+// acesse as chaves do último morador e imprima no seguinte formato
+// “O morador do bloco 2 de nome Zoey Brooks mora no 1° andar, apartamento 101”.
+const blocoDois = moradores.blocoDois
+const ultimoMorador = blocoDois[blocoDois.length-1]
+const nomeCompleto = ultimoMorador.nome + ' ' + ultimoMorador.sobrenome;
+
+console.log(
+  'O morador do bloco 2 de nome', nomeCompleto, 'mora no',
+  ultimoMorador.andar + '° andar, apartamento', ultimoMorador.apartamento
+);
+
+
+// Utilize o for para imprimir o nome completo de todos os moradores
+for (let bloco in moradores) {
+  for (let morador of moradores[bloco]) {
+    console.log(morador.nome, morador.sobrenome);
+  }
+}
