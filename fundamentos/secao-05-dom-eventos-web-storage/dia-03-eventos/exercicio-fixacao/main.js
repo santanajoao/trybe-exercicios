09c46d7b6c9f504b11c35e0de7c13f0458b7c713
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-09-30 15:53:16 -0300

    Reset in double click adicionado em todos

--- fundamentos/secao-05-dom-eventos-web-storage/dia-03-eventos/exercicio-fixacao/main.js
+++ fundamentos/secao-05-dom-eventos-web-storage/dia-03-eventos/exercicio-fixacao/main.js
@@ -65,12 +65,13 @@ function initialH3Color() {
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
+  event.target.innerText = 'Aqui está uma das tecnologias que mais gostei.';
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
 }
-
-firstLi.addEventListener('dblclick', resetText);
+for (li of allLi) {
+  li.addEventListener('dblclick', resetText);
+}
 
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
