fa8a313f64320d207787008365565c6e74a3a7f0
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-02 10:52:26 -0300

    Parte 1 resolvida

--- fundamentos/secao-05-dom-eventos-web-storage/dia-03-eventos/exercicio/script.js
+++ fundamentos/secao-05-dom-eventos-web-storage/dia-03-eventos/exercicio/script.js
@@ -1,3 +1,7 @@
+// Global variables
+
+// Fuctions
+
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
   const weekDaysList = document.querySelector('.week-days');
@@ -11,6 +15,32 @@ function createDaysOfTheWeek() {
   };
 }
 
-createDaysOfTheWeek();
+// 1 - Adicione os dias do mês no calendário
+function createDaysOfTheMonth() {
+  const decemberDaysList = [
+    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+    17, 18, 19,20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
+  const fridays = [4, 11, 18, 25];
+  const holidays = [24, 25, 31];
 
-// Escreva seu código abaixo.
+  const parentUl = document.getElementById('days');
+  for (const day of decemberDaysList) {
+    const dayLI = document.createElement('li');
+    dayLI.innerText = day;
+    dayLI.className = 'day';
+    if (fridays.includes(day)) {
+      dayLI.className += ' friday';
+    }
+    if (holidays.includes(day)) {
+      dayLI.className += ' holiday';
+    }
+
+    parentUl.appendChild(dayLI);
+  }
+}
+
+// Functions calling and code
+
+createDaysOfTheWeek();
+createDaysOfTheMonth();
