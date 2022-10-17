313741c748c9462843c11db4ef16eba601138b53
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-17 15:57:20 -0300

    Requisito 1 resolvido

---
+++ fundamentos/secao-07-js-es6-e-tests-unitarios/exercicios/exercicio-01.js
@@ -0,0 +1,14 @@
+// Substitua var por let ou const
+
+  function testingScope(escopo) {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
+
+  testingScope(true);
