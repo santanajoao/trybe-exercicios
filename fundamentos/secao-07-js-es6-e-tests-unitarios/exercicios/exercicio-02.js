49c1598f846c636b3d95719249ef41c7d763d60a
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-17 16:17:37 -0300

    Requisito 1 resolvido

---
+++ fundamentos/secao-07-js-es6-e-tests-unitarios/exercicios/exercicio-02.js
@@ -0,0 +1,5 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = (array) => array.sort((a, b) => a - b);
+
+console.log(sortOddsAndEvens(oddsAndEvens)); // será necessário alterar essa linha 😉
