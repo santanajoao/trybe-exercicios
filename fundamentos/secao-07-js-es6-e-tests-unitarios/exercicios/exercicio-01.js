00a3afa559e3e3098b41c4654d0d7cde7ef35262
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-17 16:05:15 -0300

    Exercício 01 finalizado

--- fundamentos/secao-07-js-es6-e-tests-unitarios/exercicios/exercicio-01.js
+++ fundamentos/secao-07-js-es6-e-tests-unitarios/exercicios/exercicio-01.js
@@ -5,6 +5,7 @@
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
       ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
+      console.log(`${ifScope} o que estou fazendo aqui ? :O`);
     } else {
       const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
       console.log(elseScope);
