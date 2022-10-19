0854248a725a2e9f8a4f35642f3bd5ce1f067559
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-19 19:30:55 -0300

    exercício 01 - teste 02 implementado

--- fundamentos/secao-07-js-es6-e-tests-unitarios/dia-03-jest/exercicio-01/sum.test.js
+++ fundamentos/secao-07-js-es6-e-tests-unitarios/dia-03-jest/exercicio-01/sum.test.js
@@ -1,14 +1,18 @@
 const sum = require('./sum');
 
-// Teste se o retorno de sum(4, 5) é 9
 describe('1 - Função "sum"', () => {
-  it('sum(4, 5) deve retornar 9', () => {
+  // Teste se o retorno de sum(4, 5) é 9
+  it('1.1 - sum(4, 5) deve retornar 9', () => {
     expect(sum(4, 5)).toBe(9);
   })
+
+  // Teste se o retorno de sum(0, 0) é 0
+  it('1.2 - sum(0, 0 deve retornar 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  })
+
+  // Teste se a função sum lança um erro quando os parâmetros são 4 e "5"
+
+  // Teste se a mensagem de erro é “parameters must be numbers” quando realizar
+  // a chamada sum(4, "5")
 })
-// Teste se o retorno de sum(0, 0) é 0
-
-// Teste se a função sum lança um erro quando os parâmetros são 4 e "5"
-
-// Teste se a mensagem de erro é “parameters must be numbers” quando realizar
-// a chamada sum(4, "5")
