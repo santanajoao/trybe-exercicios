c836564e1b9639535c04392da3c3fd487fa818c2
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-19 13:10:11 -0300

    Exercício estilizado

--- fundamentos/secao-07-js-es6-e-tests-unitarios/dia-02/exercicio-01/script.js
+++ fundamentos/secao-07-js-es6-e-tests-unitarios/dia-02/exercicio-01/script.js
@@ -16,21 +16,23 @@ const isNumber = (value) => {
 
 const sum = () => {
   try {
-    const term1 = document.querySelector('#first-value-input').value;
-    const term2 = document.querySelector('#second-value-input').value;
+    const term1 = document.querySelector('#first-term-input').value;
+    const term2 = document.querySelector('#second-term-input').value;
     const sum = Number(term1) + Number(term2);
     checkEmpty(term1, term2);
     isNumber(sum);
-    document.querySelector('#sum-result-p').innerHTML = `Resultado: ${sum}`;
+    document.querySelector('#sum-result').innerText = `Resultado: ${sum}`;
+    document.querySelector('#error-paragraph').innerText = '';
   } catch (error) {
     // Adicione o texto dos erros no parágrafo com id result, para que a pessoa
     // usuária saiba o que aconteceu. Lembre-se de usar erros descritivos!
-    document.querySelector('#sum-result-p').innerHTML = error.message;
+    document.querySelector('#error-paragraph').innerText = error.message;
+    document.querySelector('#sum-result').innerText = `Resultado`;
   } finally {
     // Utilize o finally para apagar os valores dos inputs ao final do bloco
     // try/catch.
-    document.querySelector('#first-value-input').value = '';
-    document.querySelector('#second-value-input').value = '';
+    document.querySelector('#first-term-input').value = '';
+    document.querySelector('#second-term-input').value = '';
   }
 }
 
