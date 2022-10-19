23a0b9e83b298676e0e417bccd752c94359aaa6e
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-19 20:13:34 -0300

    exercício 03 - teste 02 implementado

--- fundamentos/secao-07-js-es6-e-tests-unitarios/dia-03-jest/exercicio-03/fizz-buzz.test.js
+++ fundamentos/secao-07-js-es6-e-tests-unitarios/dia-03-jest/exercicio-03/fizz-buzz.test.js
@@ -6,4 +6,11 @@ describe('3 - Função "myFizzBuzz"', () => {
   it('teste se myFizzBuzz(15) retorna "fizzbuzz"', () => {
     expect(myFizzBuzz(15)).toBe('fizzbuzz');
   });
+  // Faça uma chamada com um número divisível por 3 e verifique
+  // se o retorno é o esperado
+  it('teste se myFizzBuzz(6) retorna "fizz"', () => {
+    expect(myFizzBuzz(6)).toBe('fizz');
+  });
+  // Faça uma chamada com um número divisível por 5 e verifique se
+  // o retorno é o esperado
 });
