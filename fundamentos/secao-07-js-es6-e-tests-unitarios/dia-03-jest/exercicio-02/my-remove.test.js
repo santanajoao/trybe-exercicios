dcd91323558cd541114401e6da78091debe6539f
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-19 19:59:09 -0300

    exercício 02 - teste 02 implementado

--- fundamentos/secao-07-js-es6-e-tests-unitarios/dia-03-jest/exercicio-02/my-remove.test.js
+++ fundamentos/secao-07-js-es6-e-tests-unitarios/dia-03-jest/exercicio-02/my-remove.test.js
@@ -1,9 +1,13 @@
 const myRemove = require('./my-remove');
 
-
-// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
 describe('2 - Função "myRemove"', () => {
+  // Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
   it('testa se myRemove([1, 2, 3, 4], 3) retorna o valor experado', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-  })
+  });
+  // Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array
+  // [1, 2, 3, 4]
+  it('testa se myRemove([1, 2, 3, 4], 3) não retorna o mesmo array ', () => {
+    expect([myRemove([1, 2, 3, 4], 3)]).not.toEqual([1, 2, 3, 4]);
+  });
 });
