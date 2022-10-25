343724034308bb1d3019c42ce8654001fd35ca60
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-25 15:14:52 -0300

    Funções refatoradas em uma linha

--- fundamentos/secao-08-higher-order-functions/dia-02-array-hofs/exercicios-fixacao/exercicio-03.js
+++ fundamentos/secao-08-higher-order-functions/dia-02-array-hofs/exercicios-fixacao/exercicio-03.js
@@ -1,7 +1,5 @@
 const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
-const findNameWithFiveLetters = () => {
-  return names.find((name) => name.length === 5);
-};
+const findNameWithFiveLetters = () => names.find((name) => name.length === 5);
 
 console.log(findNameWithFiveLetters());
--- fundamentos/secao-08-higher-order-functions/dia-02-array-hofs/exercicios-fixacao/exercicio-04.js
+++ fundamentos/secao-08-higher-order-functions/dia-02-array-hofs/exercicios-fixacao/exercicio-04.js
@@ -4,8 +4,6 @@ const musicas = [
   { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
 ];
 
-function findMusic(id) {
-  return musicas.find((object) => object.id === id);
-};
+const findMusic = (id) =>  musicas.find((object) => object.id === id);
 
 console.log(findMusic('31031685'));
