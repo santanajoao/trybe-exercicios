3353853925c84bdaf8b211343ac544d1ce4efb83
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-26 18:01:07 -0300

    exercício 05 resolvido

--- fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/testes/exercicio-05.test.js
+++ fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/testes/exercicio-05.test.js
@@ -1,7 +1,14 @@
 const books = require('../books-data');
+const fantasyOrScienceFictionAuthors = require('../exercicios/exercicio-05');
 
 describe('testa a função []', () => {
   it('testa se [] retorna o valor esperado', () => {
-    expect().toEqual(expectedResult);
+    const expectedResult = [
+      'Frank Herbert',
+      'George R. R. Martin',
+      'Isaac Asimov',
+      'J. R. R. Tolkien',
+    ];
+    expect(fantasyOrScienceFictionAuthors(books)).toEqual(expectedResult);
   });
 });
