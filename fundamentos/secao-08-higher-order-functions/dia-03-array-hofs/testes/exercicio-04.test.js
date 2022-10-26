2677b0fbfd4a6dc9f3403787fb57a6b8bb2cca0b
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-26 17:46:39 -0300

    exercício 04 resolvido

--- fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/testes/exercicio-04.test.js
+++ fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/testes/exercicio-04.test.js
@@ -1,7 +1,31 @@
 const books = require('../books-data');
+const oldBooksOrdered = require('../exercicios/exercicio-04');
 
 describe('testa a função []', () => {
   it('testa se [] retorna o valor esperado', () => {
-    expect().toEqual(expectedResult);
+    const expectedResult = [
+      {
+        id: 6,
+        name: 'O Chamado de Cthulhu',
+        genre: 'Terror',
+        author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+        releaseYear: 1928,
+      },
+      {
+        id: 3,
+        name: 'Fundação',
+        genre: 'Ficção Científica',
+        author: { name: 'Isaac Asimov', birthYear: 1920 },
+        releaseYear: 1951,
+      },
+      {
+        id: 2,
+        name: 'O Senhor dos Anéis',
+        genre: 'Fantasia',
+        author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+        releaseYear: 1954,
+      },
+    ];
+    expect(oldBooksOrdered(books)).toEqual(expectedResult);
   });
 });
