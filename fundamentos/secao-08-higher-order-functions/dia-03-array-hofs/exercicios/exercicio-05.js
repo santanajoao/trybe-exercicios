af76145d9f2bec55a447685de0b4c26ec31c9f46
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-26 18:28:04 -0300

    exercício 06 resolvido

--- fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/exercicios/exercicio-05.js
+++ fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/exercicios/exercicio-05.js
@@ -1,11 +1,11 @@
 const books = require('../books-data');
-// Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
 
 const isFantasyOrScienceFiction = (book) =>
   book.genre === 'Fantasia' || book.genre === 'Ficção Científica';
 
 const getAuthorName = (book) => book.author.name;
 
+// Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
 const fantasyOrScienceFictionAuthors = (booksArray) =>
   booksArray.filter(isFantasyOrScienceFiction).map(getAuthorName).sort();
 
--- fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/testes/exercicio-06.test.js
+++ fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/testes/exercicio-06.test.js
@@ -1,7 +1,9 @@
 const books = require('../books-data');
+const authorWith3DotsOnName = require('../exercicios/exercicio-06');
 
 describe('testa a função []', () => {
   it('testa se [] retorna o valor esperado', () => {
-    expect().toEqual(expectedResult);
+    const expectedResult = 'O Senhor dos Anéis';
+    expect(authorWith3DotsOnName(books)).toEqual(expectedResult);
   });
 });
