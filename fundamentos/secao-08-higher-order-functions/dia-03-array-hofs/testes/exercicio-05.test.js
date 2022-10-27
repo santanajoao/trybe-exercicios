d8328e7738541d0c5931964dcad87a84dd517096
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-27 15:00:50 -0300

    textos de descrição dos testes corrigidos

--- fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/testes/exercicio-05.test.js
+++ fundamentos/secao-08-higher-order-functions/dia-03-array-hofs/testes/exercicio-05.test.js
@@ -1,8 +1,8 @@
 const books = require('../books-data');
 const fantasyOrScienceFictionAuthors = require('../exercicios/exercicio-05');
 
-describe('testa a função []', () => {
-  it('testa se [] retorna o valor esperado', () => {
+describe('testa a função fantasyOrScienceFictionAuthors', () => {
+  it('testa se fantasyOrScienceFictionAuthors retorna o valor esperado', () => {
     const expectedResult = [
       'Frank Herbert',
       'George R. R. Martin',
