d17b4ad2a4a19ae29e374ff3b5cad6cbecee26eb
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-10-28 18:43:51 -0300

    valor inicial adicionado no reduce

--- fundamentos/secao-08-higher-order-functions/dia-05-spread-operator-rest-destructuring/exercicios/exercicio-02.js
+++ fundamentos/secao-08-higher-order-functions/dia-05-spread-operator-rest-destructuring/exercicios/exercicio-02.js
@@ -1,4 +1,4 @@
 // Crie uma função sum que dado um número ilimitado de parâmetros retorna a soma desses parâmetros. Ao chamar a função dessa forma : sum(4,5,6) o número 15 deve ser retornado.
-const sum = (...numbers) => numbers.reduce((sum, number) => sum + number);
+const sum = (...numbers) => numbers.reduce((sum, number) => sum + number, 0);
 
 console.log(sum(4, 5, 6));
