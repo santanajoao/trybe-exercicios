ada293f06e036cbca9f717619bf7891aa11f6ad3
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-11-09 09:40:52 -0300

    correções linter trybe

--- front-end/secao-01-introducao-front-end-e-js-assincrono/exercicios/field-validator/src/main.js
+++ front-end/secao-01-introducao-front-end-e-js-assincrono/exercicios/field-validator/src/main.js
@@ -1,8 +1,8 @@
-import isEmail from "validator/lib/isEmail";
-import isPostalCode from "validator/lib/isPostalCode";
-import isDate from "validator/lib/isDate";
-import isCreditCard from "validator/lib/isCreditCard";
-import isStrongPassword from "validator/lib/isStrongPassword";
+import isEmail from 'validator/lib/isEmail';
+import isPostalCode from 'validator/lib/isPostalCode';
+import isDate from 'validator/lib/isDate';
+import isCreditCard from 'validator/lib/isCreditCard';
+import isStrongPassword from 'validator/lib/isStrongPassword';
 import './style.css';
 
 const targetInputEl = document.querySelector('#validation-target');
@@ -14,14 +14,13 @@ const validateStrongPassword = (password) => {
   const isStrong = isStrongPassword(password);
   const score = isStrongPassword(password, { returnScore: true });
   const result = isStrong ? '' : 'não ';
-  messageParagraphEl.innerText =
-    `Sua senha ${result}é forte!\nPontuação: ${score}`;
+  messageParagraphEl.innerText = `Sua senha ${result}é forte!\nPontuação: ${score}`;
 };
 
 const validateEmail = (email) => {
   const result = isEmail(email) ? 'válido' : 'inválido';
   messageParagraphEl.innerText = `Email ${result}!`;
-}
+};
 
 const validateCreditCard = (card) => {
   const result = isCreditCard(card) ? 'válido' : 'inválido';
@@ -32,15 +31,15 @@ const validateDate = (date) => {
   const result = isDate(date, { format: 'DD/MM/YYYY' }) ? 'válida' : 'inválida';
   messageParagraphEl.innerText = `Data ${result}!\n`;
   if (result === 'inválida') {
-    messageParagraphEl.innerText 
-      += `A data deve ser no padrão DD/MM/YYYY e separada por "/" ou "-"`;
+    messageParagraphEl.innerText
+      += 'A data deve ser no padrão DD/MM/YYYY e separada por \'/\' ou \'-\'';
   }
 };
 
 const validatePostalCode = (code) => {
   const result = isPostalCode(code, 'BR') ? 'válido' : 'inválido';
   messageParagraphEl.innerText = `CEP ${result}\n`
-    + `É necessário adicionar os hifens "-"`;
+    + 'É necessário adicionar os hifens "-"';
 };
 
 validateButtonEl.addEventListener('click', (event) => {
@@ -49,9 +48,9 @@ validateButtonEl.addEventListener('click', (event) => {
   const { value: validationType } = selectValidationTypeEl;
   const typeAndFunction = {
     'strong-password': () => validateStrongPassword(validationTarget),
-    'email': () => validateEmail(validationTarget),
+    email: () => validateEmail(validationTarget),
     'credit-card': () => validateCreditCard(validationTarget),
-    'date': () => validateDate(validationTarget),
+    date: () => validateDate(validationTarget),
     'zip-code': () => validatePostalCode(validationTarget),
   };
   const validationFunction = typeAndFunction[validationType];
