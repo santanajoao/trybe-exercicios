19c5f00e3dcd5c6b093353e28191efd7d0c2a254
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-11-11 10:37:30 -0300

    exercício finalizado

--- front-end/secao-01-introducao-front-end-e-js-assincrono/dia-02-assincrono-promises-fetch/exercicio-bonus/select-pet/src/main.js
+++ front-end/secao-01-introducao-front-end-e-js-assincrono/dia-02-assincrono-promises-fetch/exercicio-bonus/select-pet/src/main.js
@@ -2,3 +2,42 @@ import './style.css';
 
 const DOGS_API = 'https://dog.ceo/api/breeds/image/random';
 const CATS_API = 'https://aws.random.cat/meow';
+
+const dogBtnEl = document.querySelector('#random-dog-btn');
+const catBtnEl = document.querySelector('#random-cat-btn');
+const surpriseBtnEl = document.querySelector('#surprise-btn');
+const petImgEl = document.querySelector('#pet-img');
+const placeholderSpanEl = document.querySelector('.placeholder-text');
+
+const fetchAPI = (URL) => fetch(URL).then((response) => response.json());
+
+const renderImage = () => {
+  petImgEl.style.display = 'inline';
+  placeholderSpanEl.style.display = 'none';
+};
+
+dogBtnEl.addEventListener('click', () => {
+  fetchAPI(DOGS_API).then(({ message }) => {
+    petImgEl.src = message;
+    renderImage();
+  });
+});
+
+catBtnEl.addEventListener('click', () => {
+  fetchAPI(CATS_API).then(({ file }) => {
+    petImgEl.src = file;
+    renderImage();
+  });
+});
+
+surpriseBtnEl.addEventListener('click', () => {
+  Promise.race([fetchAPI(DOGS_API), fetchAPI(CATS_API)])
+    .then((data) => {
+      if ('file' in data) {
+        petImgEl.src = data.file;
+      } else {
+        petImgEl.src = data.message;
+      }
+      renderImage();
+    });
+});
