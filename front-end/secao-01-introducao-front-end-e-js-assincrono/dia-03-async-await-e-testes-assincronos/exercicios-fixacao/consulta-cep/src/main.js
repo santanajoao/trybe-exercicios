57fd51134eeff2fec26d1e42e50a8e0d282fa2d1
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-11-11 16:02:14 -0300

    tratamentos de erro adicionados

--- front-end/secao-01-introducao-front-end-e-js-assincrono/dia-03-async-await-e-testes-assincronos/exercicios-fixacao/consulta-cep/src/main.js
+++ front-end/secao-01-introducao-front-end-e-js-assincrono/dia-03-async-await-e-testes-assincronos/exercicios-fixacao/consulta-cep/src/main.js
@@ -1,8 +1,17 @@
+import Swal from 'sweetalert2';
 import './style.css';
 
 const inputCEPEl = document.querySelector('input');
 const searchBtnEl = document.querySelector('button');
 const displayParagraphEl = document.querySelector('p');
+
+const throwSweetError = (message) => {
+  Swal.fire({ icon: 'error', title: message });
+};
+
+const renderCEPData = (data) => {
+  displayParagraphEl.textContent = data;
+};
 
 searchBtnEl.addEventListener('click', async () => {
   try {
@@ -10,10 +19,15 @@ searchBtnEl.addEventListener('click', async () => {
     const API_PATH = `https://viacep.com.br/ws/${endpoint}/json/`;
     const response = await fetch(API_PATH);
     const data = await response.json();
-    displayParagraphEl.innerText = data;
-    console.log(data);
+
+    if ('erro' in data) {
+      throwSweetError('CEP inexistente!');
+    } else {
+      renderCEPData(data);
+    }
   } catch (error) {
-    console.log(error);
-    displayParagraphEl.innerText = error.message;
+    throwSweetError('O text digitado não é um CEP!');
+  } finally {
+    inputCEPEl.value = '';
   }
 });
