93d18636ef14fbdd4f8a4d8b6cee93c7e16406f1
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-11-10 21:35:55 -0300

    mensagem de erro substituída por tentar chamar outro id

--- front-end/secao-01-introducao-front-end-e-js-assincrono/dia-02-assincrono-promises-fetch/exercicios/superheroes/src/main.js
+++ front-end/secao-01-introducao-front-end-e-js-assincrono/dia-02-assincrono-promises-fetch/exercicios/superheroes/src/main.js
@@ -1,4 +1,3 @@
-import Swal from 'sweetalert2';
 import './style.css';
 
 const heroImgEl = document.querySelector('#hero-img');
@@ -8,15 +7,17 @@ const drawnHeroBtnEl = document.querySelector('.draw-btn');
 // Retorna um número entre 1 e 731 que representa um id
 const getRandomId = () => Math.floor(Math.random() * 731) + 1;
 
-drawnHeroBtnEl.addEventListener('click', () => {
+const fetchAndRender = () => {
   const id = getRandomId();
   const URL = `https://akabab.github.io/superhero-api/api/id/${id}.json`;
   fetch(URL)
     .then((response) => response.json())
-    .catch(() => Swal.fire('Erro ao encontrar personagem!'))
+    .catch(() => fetchAndRender())
     .then(({ images, name }) => {
       heroImgEl.src = images.sm;
       heroImgEl.style.display = 'inline';
       heroNameH2El.textContent = name;
     });
-});
+};
+
+drawnHeroBtnEl.addEventListener('click', fetchAndRender);
