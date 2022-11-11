f63af899f9713b5981244a6daa0802388dd9182c
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-11-11 15:48:38 -0300

    script inicial adicionado

--- front-end/secao-01-introducao-front-end-e-js-assincrono/dia-03-async-await-e-testes-assincronos/exercicios-fixacao/consulta-cep/src/main.js
+++ front-end/secao-01-introducao-front-end-e-js-assincrono/dia-03-async-await-e-testes-assincronos/exercicios-fixacao/consulta-cep/src/main.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const inputCEPEl = document.querySelector('input');
+const searchBtnEl = document.querySelector('button');
+const displayParagraphEl = document.querySelector('p');
+
+searchBtnEl.addEventListener('click', async () => {
+  try {
+    const endpoint = inputCEPEl.value;
+    const API_PATH = `https://viacep.com.br/ws/${endpoint}/json/`;
+    const response = await fetch(API_PATH);
+    const data = await response.json();
+    displayParagraphEl.innerText = data;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    displayParagraphEl.innerText = error.message;
+  }
+});
