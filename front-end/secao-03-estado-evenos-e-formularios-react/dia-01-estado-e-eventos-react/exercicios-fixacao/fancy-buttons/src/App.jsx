0618edd2e4b014ea0f24772a7b556289e671eaa4
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-11-29 19:35:55 -0300

    requisito 2 da parte 1 do exercício de fixação

--- front-end/secao-03-estado-evenos-e-formularios-react/dia-01-estado-e-eventos-react/exercicios-fixacao/fancy-buttons/src/App.jsx
+++ front-end/secao-03-estado-evenos-e-formularios-react/dia-01-estado-e-eventos-react/exercicios-fixacao/fancy-buttons/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  printOnClick = () => console.log('click');
+  leftBtnConsole = () => console.log('Nesse não!');
+
+  middleBtnConsole = () => console.log('Clicou!');
+
+  rightBtnConsole = () => console.log('Não é esse!');
 
   render() {
     return (
       <div className="App">
-        <button onClick={ this.printOnClick }>Clique aqui!</button>
+        <button onClick={ this.leftBtnConsole }>Clique aqui &gt;</button>
+        <button onClick={ this.middleBtnConsole }>Clique aqui!</button>
+        <button onClick={ this.rightBtnConsole }>&lt; Clique aqui</button>
       </div>
     )
   }
