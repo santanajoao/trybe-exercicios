363e590934a3b372a1ba07ae5d8ede90e6f41c99
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-07 13:38:00 -0300

    cria utilitário que lê arquivos json

---
+++ back-end/secao-04-introducao-ao-nodejs/dia-01-runtime-assincrono/src/utils/json.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function readJSON(filePath) {
+  try {
+    const resolvedPath = path.resolve(__dirname, filePath);
+    const response = await fs.readFile(resolvedPath);
+    const data = JSON.parse(response);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
