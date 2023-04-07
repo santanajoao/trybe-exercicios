5aee0e3403face4c8e4a4b5b076b5907051e00c8
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-07 13:58:26 -0300

    resolve a parte 2 do exercício 6

--- back-end/secao-04-introducao-ao-nodejs/dia-01-runtime-assincrono/src/simpsons.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-01-runtime-assincrono/src/simpsons.js
@@ -1,8 +1,27 @@
 const { readJSON } = require('./utils/json');
 
+const DATA_FILE_PATH = '../data/simpsons.json';
+
 async function printSimpsonsCharacters() {
-  const charactersList = await readJSON('../data/simpsons.json');
+  const charactersList = await readJSON(DATA_FILE_PATH);
   charactersList.forEach(({ id, name }) => console.log(`${id} - ${name}`));
 }
 
-printSimpsonsCharacters();
+async function getCharacterNameById(id) {
+  const charactersList = await readJSON(DATA_FILE_PATH);
+  const matchingCharacter = charactersList.find(
+    (character) => Number(character.id) === id,
+  );
+
+  if (!matchingCharacter) {
+    throw new Error('Id not found');
+  }
+
+  return matchingCharacter;
+}
+
+async function main() {
+  printSimpsonsCharacters();
+}
+
+main();
--- back-end/secao-04-introducao-ao-nodejs/dia-01-runtime-assincrono/src/utils/json.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-01-runtime-assincrono/src/utils/json.js
@@ -2,14 +2,9 @@ const fs = require('fs').promises;
 const path = require('path');
 
 async function readJSON(filePath) {
-  try {
-    const resolvedPath = path.resolve(__dirname, filePath);
-    const response = await fs.readFile(resolvedPath);
-    const data = JSON.parse(response);
-    return data;
-  } catch (error) {
-    console.log(error.message);
-  }
+  const resolvedPath = path.resolve(__dirname, filePath);
+  const response = await fs.readFile(resolvedPath);
+  return JSON.parse(response);
 }
 
 module.exports = { readJSON };
