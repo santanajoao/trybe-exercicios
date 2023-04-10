cceb56956c11be37073b03b1f9f95cfd5046a572
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-10 16:39:44 -0300

    exercicio 5: cria endpoint get /movies/:id

--- back-end/secao-04-introducao-ao-nodejs/dia-02-api-rest-com-express/src/app.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-02-api-rest-com-express/src/app.js
@@ -1,7 +1,23 @@
+const path = require('path');
 const express = require('express');
+const { readJSON } = require('./utils/json');
+
+const MOVIES_JSON_PATH = path.resolve(__dirname, './data/movies.json');
+
+const OK = 200;
+const NOT_FOUND = 404;
 
 const app = express();
 
-app.get('/', (_, res) => res.status(200).json({ message: 'all ok' }));
+app.get('/movies/:id', async (req, res) => {
+  const requestedId = Number(req.params.id);
+  const movieList = await readJSON(MOVIES_JSON_PATH);
+
+  const requestedMovie = movieList.find((movie) => movie.id === requestedId);
+  if (requestedMovie) {
+    res.status(OK).json({ movie: requestedMovie });
+  }
+  res.status(NOT_FOUND).json({ message: 'Movie not found' });
+});
 
 module.exports = app;
