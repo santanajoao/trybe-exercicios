2d42ee7d3d125c0ea6ecf77acc72f9555d3ff6ae
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-10 17:39:43 -0300

    corrige status code do post e adiciona try catch

--- back-end/secao-04-introducao-ao-nodejs/dia-02-api-rest-com-express/src/app.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-02-api-rest-com-express/src/app.js
@@ -2,33 +2,47 @@ const express = require('express');
 const { getMovies, addMovie } = require('./helpers/movies');
 
 const OK = 200;
+const CREATED = 201;
 const NOT_FOUND = 404;
+const INTERNAL_SERVER_ERROR = 500;
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/movies', async (_, res) => {
-  const movieList = await getMovies();
-  res.status(OK).json({ movies: movieList });
+  try {
+    const movieList = await getMovies();
+    res.status(OK).json({ movies: movieList });
+  } catch (error) {
+    res.status(INTERNAL_SERVER_ERROR).json({ message: error.message });
+  }
 });
 
 app.get('/movies/:id', async (req, res) => {
-  const requestedId = Number(req.params.id);
-  const movieList = await getMovies();
+  try {
+    const requestedId = Number(req.params.id);
+    const movieList = await getMovies();
 
-  const requestedMovie = movieList.find((movie) => movie.id === requestedId);
-  if (requestedMovie) {
-    res.status(OK).json({ movie: requestedMovie });
-  } else {
-    res.status(NOT_FOUND).json({ message: 'Movie not found' });
+    const requestedMovie = movieList.find((movie) => movie.id === requestedId);
+    if (requestedMovie) {
+      res.status(OK).json({ movie: requestedMovie });
+    } else {
+      res.status(NOT_FOUND).json({ message: 'Movie not found' });
+    }
+  } catch (error) {
+    res.status(INTERNAL_SERVER_ERROR).json({ message: error.message });
   }
 });
 
 app.post('/movies', async (req, res) => {
-  const { movie, price } = req.body;
-  const newMovie = await addMovie(movie, price);
-  res.status(OK).json({ movie: newMovie });
+  try {
+    const { movie, price } = req.body;
+    const newMovie = await addMovie(movie, price);
+    res.status(CREATED).json({ movie: newMovie });
+  } catch (error) {
+    res.status(INTERNAL_SERVER_ERROR).json({ message: error.message });
+  }
 });
 
 module.exports = app;
