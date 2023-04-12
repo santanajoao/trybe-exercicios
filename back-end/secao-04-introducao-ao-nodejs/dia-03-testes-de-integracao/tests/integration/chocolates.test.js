19f375b5835438f2c644fd5ae8e439cc15326284
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-12 15:04:19 -0300

    implementa o segundo teste tdd

--- back-end/secao-04-introducao-ao-nodejs/dia-03-testes-de-integracao/tests/integration/chocolates.test.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-03-testes-de-integracao/tests/integration/chocolates.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const sinon = require('sinon');
@@ -12,12 +13,28 @@ const { expect } = chai;
 const OK = 200;
 
 describe('chocolates API', () => {
-  it('GET /chocolates gives a array with all chocolates', async () => {
+  afterEach(sinon.restore);
+
+  it('GET /chocolates gives an array with all chocolates', async () => {
     sinon.stub(fs.promises, 'readFile').resolves(JSON.stringify(mocks.all));
 
     const response = await chai.request(app).get('/chocolates');
     expect(fs.promises.readFile.called).to.be.equal(true);
     expect(response.status).to.be.equal(OK);
     expect(response.body.chocolates).to.be.deep.equal(mocks.chocolates);
+  });
+
+  it('GET /chocolates/:id gives a object with chocolate infos', async () => {
+    sinon.stub(fs.promises, 'readFile').resolves(JSON.stringify(mocks.all));
+
+    const chocolateMock = mocks.chocolates[1];
+
+    const response = await chai
+      .request(app)
+      .get(`/chocolates/${chocolateMock.id}`);
+
+    expect(fs.promises.readFile.called).to.be.equal(true);
+    expect(response.status).to.be.equal(OK);
+    expect(response.body.chocolate).to.be.deep.equal(chocolateMock);
   });
 });
