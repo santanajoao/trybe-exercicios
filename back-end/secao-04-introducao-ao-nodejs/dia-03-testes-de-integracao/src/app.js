3c4a7f4d324142cf0909670e4ab36d0d99660c1d
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-12 12:47:19 -0300

    criar a primeira rota da api tdd

--- back-end/secao-04-introducao-ao-nodejs/dia-03-testes-de-integracao/src/app.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-03-testes-de-integracao/src/app.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const { getChocolates } = require('./helpers/chocolates');
 
 const app = express();
+const OK = 200;
+const INTERNAL_SERVER_ERROR = 500;
+
+app.get('/chocolates', async (_, res) => {
+  try {
+    const chocolates = await getChocolates();
+    res.status(OK).json({ chocolates });
+  } catch (error) {
+    res.status(INTERNAL_SERVER_ERROR).json({ message: error.message });
+  }
+});
 
 module.exports = app;
