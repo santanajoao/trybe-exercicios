82795c3040d7f9da18e65200a25c6d1572edd82e
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-12 12:46:43 -0300

    implementa os testes tdd

---
+++ back-end/secao-04-introducao-ao-nodejs/dia-03-testes-de-integracao/tests/integration/chocolates.test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../../src/app');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+const OK = 200;
+
+describe('chocolates API', () => {
+  it('GET /chocolates', async () => {
+    const response = await chai.request(app).get('/chocolates');
+    expect(response.status).to.be.equal(OK);
+  });
+});
