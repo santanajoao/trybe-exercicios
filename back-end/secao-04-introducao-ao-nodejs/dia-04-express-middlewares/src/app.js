69bdd3d5da73e49eb56c0ef001edcb4e0db10e2b
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-13 10:23:26 -0300

    instala os middlewares de erro, seguranca e json

--- back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/app.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
+
+require('express-async-errors');
 
 const app = express();
+
+app.use(helmet());
+app.use(express.json());
 
 module.exports = app;
