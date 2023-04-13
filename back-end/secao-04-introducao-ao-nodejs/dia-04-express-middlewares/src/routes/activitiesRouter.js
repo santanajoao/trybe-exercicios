dc0da792b21e7b10a549a1f3434e2a69998fc6fd
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-13 10:39:42 -0300

    cria middleware de validação de nome

--- back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/routes/activitiesRouter.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/routes/activitiesRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const validateName = require('../middlewares/activities/validateName');
 
 const router = express.Router();
 
 const CREATED = 201;
 
-router.post('/', (_, res) => {
+router.post('/', validateName, (_, res) => {
   res.status(CREATED).json({ message: 'Atividade cadastrada com sucesso' });
 });
 
