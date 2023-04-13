fa3f67153e9b515fb36074691ae4036d039093bb
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-13 11:50:57 -0300

    cria middleware de validação da description

--- back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/routes/activitiesRouter.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/routes/activitiesRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const validateName = require('../middlewares/validateName');
 const validatePrice = require('../middlewares/validatePrice');
+const validateDescription = require('../middlewares/validateDescription');
 
 const router = express.Router();
 
 const CREATED = 201;
 
-router.post('/', validateName, validatePrice, (_, res) => {
+router.post('/', validateName, validatePrice, validateDescription, (_, res) => {
   res.status(CREATED).json({ message: 'Atividade cadastrada com sucesso' });
 });
 
