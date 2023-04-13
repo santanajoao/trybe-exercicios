669c4f8cff2778ac3a891bd593db747909a6bddb
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-13 12:02:19 -0300

    cria middleware de validação de createdAt

--- back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/routes/activitiesRouter.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/routes/activitiesRouter.js
@@ -2,13 +2,21 @@ const express = require('express');
 const validateName = require('../middlewares/validateName');
 const validatePrice = require('../middlewares/validatePrice');
 const validateDescription = require('../middlewares/validateDescription');
+const validateCreatedAt = require('../middlewares/validateCreatedAt');
 
 const router = express.Router();
 
 const CREATED = 201;
 
-router.post('/', validateName, validatePrice, validateDescription, (_, res) => {
-  res.status(CREATED).json({ message: 'Atividade cadastrada com sucesso' });
-});
+router.post(
+  '/',
+  validateName,
+  validatePrice,
+  validateDescription,
+  validateCreatedAt,
+  (_, res) => {
+    res.status(CREATED).json({ message: 'Atividade cadastrada com sucesso' });
+  },
+);
 
 module.exports = router;
