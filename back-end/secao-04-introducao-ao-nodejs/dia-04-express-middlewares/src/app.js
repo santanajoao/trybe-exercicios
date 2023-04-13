589801fd2cff7d1465f7762681c261d829b7c868
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-13 10:46:39 -0300

    cria middleware de erro

--- back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/app.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/app.js
@@ -4,10 +4,23 @@ require('express-async-errors');
 const activitiesRouter = require('./routes/activitiesRouter');
 
 const app = express();
+const BAD_REQUEST = 500;
 
 app.use(helmet());
 app.use(express.json());
 
 app.use('/activities', activitiesRouter);
+
+app.use((err, _req, res, _next) => {
+  const { status = BAD_REQUEST, message } = err;
+
+  if (status === BAD_REQUEST) {
+    console.error(message);
+    return res.status(BAD_REQUEST).json({
+      message: message`Sorry! Some internal error ocurred: ${message}`,
+    });
+  }
+  res.status(status).json({ message });
+});
 
 module.exports = app;
--- back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/middlewares/activities/validateName.js
+++ back-end/secao-04-introducao-ao-nodejs/dia-04-express-middlewares/src/middlewares/activities/validateName.js
@@ -2,14 +2,17 @@ const BAD_REQUEST = 400;
 
 function validateName(req, _res, next) {
   const MIN_LENGTH = 5;
-  const { name } = req.body;
 
+  const { name } = req.body;
   if (!name) {
-    next({ status: BAD_REQUEST, message: 'O campo "name" é obrigatório' });
+    return next({
+      status: BAD_REQUEST,
+      message: 'O campo "name" é obrigatório',
+    });
   }
 
   if (name.length < MIN_LENGTH) {
-    next({
+    return next({
       status: BAD_REQUEST,
       message: `O campo "name" deve ter pelo menos ${MIN_LENGTH} caracteres`,
     });
