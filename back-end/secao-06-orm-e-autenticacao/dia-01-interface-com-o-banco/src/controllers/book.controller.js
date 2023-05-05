e07860b96cd5aafaecf25b5a2d304790ebd034a5
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-05 17:19:29 -0300

    corrige service e controller para retorno { type, message }

--- back-end/secao-06-orm-e-autenticacao/dia-01-interface-com-o-banco/src/controllers/book.controller.js
+++ back-end/secao-06-orm-e-autenticacao/dia-01-interface-com-o-banco/src/controllers/book.controller.js
@@ -1,8 +1,11 @@
 const { BookService } = require('../services');
 
-const handleGetAllBooks = (_req, res) => {
-  const books = BookService.findAllBooks();
-  res.status(200).json(books)
+const handleGetAllBooks = async (_req, res) => {
+  const { type, message } = await BookService.findAllBooks();
+  if (type) {
+    return res.status(200).json({ message });
+  }
+  res.status(200).json(message);
 };
 
 module.exports = {
--- back-end/secao-06-orm-e-autenticacao/dia-01-interface-com-o-banco/src/services/book.service.js
+++ back-end/secao-06-orm-e-autenticacao/dia-01-interface-com-o-banco/src/services/book.service.js
@@ -2,9 +2,18 @@ const { Book } = require('../models');
 
 const findAllBooks = async () => {
   const books = await Book.findAll();
-  return books;
+  return { type: null, message: books };
+};
+
+const findBookById = async (bookId) => {
+  const book = await Book.findByPk(bookId);
+  if (!book) {
+    return { type: 'BOOK_NOT_FOUND', message: 'Book not found' };
+  }
+  return { type: null, message: book };
 };
 
 module.exports = {
   findAllBooks,
+  findBookById,
 };
