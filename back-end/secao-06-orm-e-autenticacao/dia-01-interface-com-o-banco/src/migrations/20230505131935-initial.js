9f50ddfb0dafac419784330ab81719619c4d3696
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-05 14:31:05 -0300

    cria o model de book

---
+++ back-end/secao-06-orm-e-autenticacao/dia-01-interface-com-o-banco/src/migrations/20230505131935-initial.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      }
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
--- back-end/secao-06-orm-e-autenticacao/dia-01-interface-com-o-banco/src/server.js
+++ back-end/secao-06-orm-e-autenticacao/dia-01-interface-com-o-banco/src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const PORT = 3001;
+const PORT = process.env.NODE_PORT;
 
 app.listen(PORT, () => console.log(`Express server on port ${PORT}`));
