1563cb2013c7480d173c02225e0095ea706d576d
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-29 23:07:06 -0300

    exercício 05

---
+++ back-end/secao-09-OOP-e-SOLID/dia-01-introducao-a-oop/src/entities/Date.ts
@@ -0,0 +1,11 @@
+export default class Date {
+  day: number;
+  month: number;
+  year: number;
+
+  constructor(day: number, month: number, year: number) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+}
