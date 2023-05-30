bc98ebabc8a552b49196bea1e084be10091919fa
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-29 22:26:22 -0300

    exercicio 02

--- back-end/secao-09-OOP-e-SOLID/dia-01-introducao-a-oop/src/entities/Student.ts
+++ back-end/secao-09-OOP-e-SOLID/dia-01-introducao-a-oop/src/entities/Student.ts
@@ -1,11 +1,24 @@
 export default class Student {
   enrollment: string;
   name: string;
-  testsGrades: number[] = [];
+  examsGrades: number[] = [];
   assignmentGrades: number[] = [];
+
+  static EXAMS_NUMBER = 4;
+  static ASSIGNMENTS_NUMBER = 2;
 
   constructor(enrollment: string, name: string) {
     this.enrollment = enrollment;
     this.name = name;
+  }
+
+  getGradesSum(): number {
+    const allGrades = [...this.examsGrades, ...this.assignmentGrades];
+    return allGrades.reduce((sum, grade) => sum + grade);
+  }
+
+  getGradesAverage(): number {
+    const gradesNumber = Student.ASSIGNMENTS_NUMBER + Student.EXAMS_NUMBER;
+    return this.getGradesSum() / gradesNumber;
   }
 }
