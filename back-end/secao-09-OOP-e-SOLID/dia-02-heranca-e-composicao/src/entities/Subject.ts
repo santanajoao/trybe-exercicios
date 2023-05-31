2498e5a136158b334684e00b009c1eed3826a3a4
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-31 10:14:31 -0300

    exercício 04

--- back-end/secao-09-OOP-e-SOLID/dia-02-heranca-e-composicao/src/entities/Subject.ts
+++ back-end/secao-09-OOP-e-SOLID/dia-02-heranca-e-composicao/src/entities/Subject.ts
@@ -1,7 +1,24 @@
 export default class Subject {
-  name: string;
-  
+  private _name: string;
+
   constructor(name: string) {
-    this.name = name;
+    this.validateName(name);
+
+    this._name = name;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(newName: string) {
+    this.validateName(newName);
+    this._name = newName;
+  }
+
+  private validateName(name: string) {
+    if (name.length < 3) {
+      throw new Error('The name must be at least three characters long');
+    }
   }
 }
