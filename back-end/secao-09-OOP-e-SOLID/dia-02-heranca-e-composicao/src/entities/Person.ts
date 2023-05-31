f07b7cf5aef989dc830ae5ced084d8957a09813b
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-31 09:59:55 -0300

    Exercício 01: adiciona getters e setters com validações

--- back-end/secao-09-OOP-e-SOLID/dia-02-heranca-e-composicao/src/entities/Person.ts
+++ back-end/secao-09-OOP-e-SOLID/dia-02-heranca-e-composicao/src/entities/Person.ts
@@ -1,39 +1,57 @@
 export default class Person {
-  name: string;
-  birthDate: Date;
+  private _name: string;
+  private _birthDate: Date;
 
   constructor(name: string, birthDate: Date) {
-    this.name = name;
-    this.birthDate = birthDate;
+    this._name = name;
+    this._birthDate = birthDate;
 
     this.validatePerson();
   }
   
-  private validateName() {
-    if (this.name.length < 3) {
+  get name() {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this.validateName(name);
+    this._name = name;
+  }
+
+  get birthDate() {
+    return this._birthDate;
+  }
+
+  set birthDate(birthDate: Date) {
+    this.validateBirthDate(birthDate);
+    this._birthDate = birthDate;
+  }
+
+  private validateName(name: string) {
+    if (name.length < 3) {
       throw new Error('The name must be at least three characters long');
     }
   }
 
-  private validateBirthDate() {
+  private validateBirthDate(birthDate: Date) {
     const currentDate = new Date();
-    if (this.birthDate > currentDate) {
+    if (birthDate > currentDate) {
       throw new Error('The birthDate cannot be a date in the future');
     }
 
-    if (this.getAge() > 120) {
+    if (this.getAge(birthDate) > 120) {
       throw new Error('The person cannot be more than 120 years old');
     }
   }
   
-  getAge() {
-    const timeDifference = Date.now() - this.birthDate.getTime();
+  getAge(birthDate: Date) {
+    const timeDifference = Date.now() - birthDate.getTime();
     const age = timeDifference / 3.154e10;
     return Math.trunc(age);
   }
   
   private validatePerson() {
-    this.validateName();
-    this.validateBirthDate();
+    this.validateName(this._name);
+    this.validateBirthDate(this._birthDate);
   }
 }
